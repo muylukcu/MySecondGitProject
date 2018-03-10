32a370b6c7171a0ffacc60703b675b871fe42b54
github@cybertekschool.com <github@cybertekschool.com>
2018-03-10 12:53:03 -0500

    added loop and print statement

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -4,6 +4,8 @@ public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good Evening Class!");
 		
-		System.out.println("My name is Maria");
+		for(int i=0; i<12; i++) {
+			System.out.println("It is still too early to go home");
+		}
 	}
 }
