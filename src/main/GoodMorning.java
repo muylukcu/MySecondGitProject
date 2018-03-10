cfe8c51b143f87742a89d929a5073b0f8500b767
github@cybertekschool.com <github@cybertekschool.com>
2018-03-10 12:55:32 -0500

    added print statement

--- src/main/GoodMorning.java
+++ src/main/GoodMorning.java
@@ -3,5 +3,7 @@ package main;
 public class GoodMorning {
 	public static void main(String[] args) {
 		System.out.println("Good Morning  Class!");
+		
+		System.out.println("Few minutes left before the break");
 	}
 }
