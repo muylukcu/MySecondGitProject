1c4ad7f9d65d06e3cc6153eefe85bffc9fe73253
github@cybertekschool.com <github@cybertekschool.com>
2018-03-10 12:21:13 -0500

    added print statement

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -3,5 +3,7 @@ package main;
 public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good Evening Class!");
+		
+		System.out.println("My name is Maria");
 	}
 }
